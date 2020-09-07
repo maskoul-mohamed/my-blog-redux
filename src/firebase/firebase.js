c9a7e4931b34a2e45c8a646dfb1f6d245184d6d2
Moe <moha1058@gmail.com>
2020-09-07 18:18:18 +0100

    Set up firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/database'; 
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBSE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBSE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBSE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBSE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBSE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBSE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBSE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBSE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+const database = firebase.database()
+
+export { firebase, database as default };
